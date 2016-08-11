da99ad524cdaf0308d615b4a5c2547189c5bed0c
Ece Ozalp <github email address>
2016-08-10 22:09:18 -0400

    matching errors

---
+++ app/application.rb
@@ -0,0 +1,25 @@
+
+
+
+class Application
+@@items = []
+  def call(env)
+    resp = Rack::Response.new
+    req = Rack::Request.new(env)
+      if req.path.match(/items/)
+        searched_item = @@items.find do |item|
+        item.name == req.path.split("/").last
+        end
+        if searched_item
+          resp.write "#{searched_item.price}"
+        else 
+          resp.write "Item not found"
+          resp.status = 400
+        end
+      else
+        resp.write "Route not found"
+        resp.status = 404
+      end
+    resp.finish
+  end
+end
